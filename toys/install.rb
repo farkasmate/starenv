4f40833ca7c84a7f4297f0ac4ff0acd6d842c972
Mate Farkas <mate.farkas@sch.hu>
2022-06-01 19:53:25 +0000

    Overwrite wrapper file on install

--- toys/install.rb
+++ toys/install.rb
@@ -68,7 +68,7 @@ end
 
 def run
   wrapper_file = File.join(BIN_DIRECTORY, ENV.fetch('ENV_CLI_NAME'))
-  create_wrapper(wrapper_file) unless File.exist? wrapper_file
+  create_wrapper(wrapper_file)
 
   FileUtils.mkdir_p(VERSIONS_DIRECTORY) unless File.exist? VERSIONS_DIRECTORY
   versions = Dir[File.join(VERSIONS_DIRECTORY, '*')].collect { |version_dir| File.basename(version_dir) }
